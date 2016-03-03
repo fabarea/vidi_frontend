1ae047bd7c65fe29bfe1591f2353fa1bb39e88cd
Alexander Grein <mail@typo3-webagentur.com>
2016-03-03 15:00:02 +0100

    Add static file config

--- ext_tables.php
+++ ext_tables.php
@@ -20,4 +20,6 @@ if (TYPO3_MODE == 'BE') {
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['vidifrontend_pi1'] = 'pi_flexform';
 
     $GLOBALS['TBE_MODULES_EXT']["xMOD_db_new_content_el"]['addElClasses']['Fab\VidiFrontend\Backend\Wizard'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('vidi_frontend') . 'Classes/Backend/Wizard.php';
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Vidi Frontend');
 }
