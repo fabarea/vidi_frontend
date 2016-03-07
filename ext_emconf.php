a656ed097a7ed1d9b359bdcd4d146e56c4367b1a
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-03-07 15:26:00 +0100

    [CLEANUP] Simplify ext_emconf file

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,35 +1,24 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "vidi".
- *
- * Auto generated 10-01-2013 17:54
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'Versatile and Interactive Display - List Component for the Frontend',
-	'description' => 'Generic List Component for the Frontend where content can be filtered in an advanced way... Veni, vidi, vici!',
+	'description' => 'Generic List Component for the Frontend where content can be filtered in an advanced way... Veni, vidi, vici!_f',
 	'author' => 'Fabien Udriot',
 	'author_email' => 'support@ecodev.ch',
 	'category' => 'plugin',
 	'author_company' => 'Ecodev',
 	'state' => 'beta',
-	'clearCacheOnLoad' => 0,
 	'version' => '1.0.0-dev',
-	'constraints' => array(
-		'depends' => array(
+	'constraints' => [
+		'depends' => [
 			'typo3' => '6.2.0-7.99.99',
 			'vidi' => '2.0.3-2.99.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'suggests' => array(
-	),
-);
+		],
+		'conflicts' => [
+		],
+		'suggests' => [
+		],
+	],
+	'suggests' => [
+	],
+];
