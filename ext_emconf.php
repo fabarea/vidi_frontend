ab8b8a367ba0f72d77cc7e8e9c5d6571b47a8830
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-03-07 15:18:16 +0100

    [TASK] Adjust dependency to Vidi

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '6.2.0-7.99.99',
-			'vidi' => '',
+			'vidi' => '2.0.3-2.99.99',
 		),
 		'conflicts' => array(
 		),
