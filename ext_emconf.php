a6fa72adecbc4abc31de919259e23b6ddb7c75de
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-04-01 17:22:34 +0200

    [RELEASE] 1.0.0
    * [FEATURE] Add more Grid options in Flexform
    * [FEATURE] Make logical separator configurable AND vs OR
    * [FEATURE] Allow to override settings by TypoScript
    * [FEATURE] Implement records mass-action
    * [CONFIG] Remove unused override following Vidi improvement
    * [FEATURE] Autoload TypoScript according to configuration
    * [TASK] Change order of parameters for RealURL
    * [BUGFIX] Plugin should be displayable twice on the same page
    * [FEATURE] Possibly limit number of records
    * [FEATURE] Enhance additional constraints with more operators
    * [BUGFIX] Click on row does not work if parameter prefix is not set
    * [BUGFIX] Hide navigation length menu for mobile layout
    * [BUGFIX] Add auto-load section in composer.json
    
    Please check the Flexform settings in the Backend
    
    https://github.com/fabarea/vidi_frontend/compare/0.9.2...1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF[$_EXTKEY] = [
 	'title' => 'Versatile and Interactive Display - List Component for the Frontend',
 	'description' => 'Generic List Component for the Frontend where content can be filtered in an advanced way... Veni, vidi, vici!_f',
 	'author' => 'Fabien Udriot',
-	'author_email' => 'support@ecodev.ch',
+	'author_email' => 'fabien@ecodev.ch',
 	'category' => 'plugin',
 	'author_company' => 'Ecodev',
 	'state' => 'beta',
-	'version' => '1.0.0-dev',
+	'version' => '1.0.0',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '6.2.0-7.99.99',
+			'typo3' => '7.6.0-7.99.99',
 			'vidi' => '2.1.0-2.99.99',
 		],
 		'conflicts' => [
