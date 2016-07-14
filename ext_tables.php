3bcb300a82cfaa924ca66f077aa5aa661d66036a
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-07-14 11:27:27 +0200

    [BUGFIX] Correctly serialize configuration in ext_tables

--- ext_tables.php
+++ ext_tables.php
@@ -3,6 +3,8 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
+$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['vidi_frontend']);
+
 // Possible Static TS loading
 if (true === isset($configuration['autoload_typoscript']['value']) && true === (bool)$configuration['autoload_typoscript']['value']) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('vidi_frontend', 'Configuration/TypoScript', 'Vidi Frontend: generic List Component');
