897b08e11fe843681b4ee10a322a6f5108c2e3c4
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-07-25 19:53:04 +0200

    Release 1.1.0 - https://github.com/fabarea/vidi_frontend/compare/1.0.0...1.1.0
    [FEATURE] Transmit dynamic parameters in the Ajax request as well
    [FEATURE] Support dynamic filtering
    [TASK] Enable respository reset default query settings
    [BUGFIX] Correctly serialize configuration in ext_tables
    [FEATURE] Configure whether to respect ignore fields or not in the BE

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,13 +2,13 @@
 
 $EM_CONF[$_EXTKEY] = [
 	'title' => 'Versatile and Interactive Display - List Component for the Frontend',
-	'description' => 'Generic List Component for the Frontend where content can be filtered in an advanced way... Veni, vidi, vici!_f',
+	'description' => 'Generic List Component for the Frontend where content can be filtered in an advanced way... Veni, vidi, vici! - demo https://speciality.ecodev.ch/content-elements/dynamic-list-files/',
 	'author' => 'Fabien Udriot',
 	'author_email' => 'fabien@ecodev.ch',
 	'category' => 'plugin',
 	'author_company' => 'Ecodev',
 	'state' => 'beta',
-	'version' => '1.1.0-dev',
+	'version' => '1.1.0',
 	'constraints' => [
 		'depends' => [
 			'typo3' => '7.6.0-7.99.99',
