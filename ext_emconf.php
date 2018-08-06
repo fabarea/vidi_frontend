fda090c1a423aca3002ae782f3db642860cf3294
Adrien Crivelli <adrien.crivelli@gmail.com>
2018-08-06 13:39:47 +0900

    [TASK] Remove mention of obsolete demo

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
 	'title' => 'Versatile and Interactive Display - List Component for the Frontend',
-	'description' => 'Generic List Component for the Frontend where content can be filtered in an advanced way... Veni, vidi, vici! - demo https://speciality.ecodev.ch/content-elements/dynamic-list-files/',
+	'description' => 'Generic List Component for the Frontend where content can be filtered in an advanced way... Veni, vidi, vici!',
 	'author' => 'Fabien Udriot',
 	'author_email' => 'fabien@ecodev.ch',
 	'category' => 'plugin',
