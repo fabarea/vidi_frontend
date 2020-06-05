aaafd75594b024c066df5cd7cb0fb4639c2650cb
Fabien Udriot <fabien@omic.ch>
2020-06-05 09:56:07 +0200

    Release 3.0.1 - <https://github.com/fabarea/vidi_frontend/compare/3.0.0...3.0.1>
    [BUGFIX] Fix version number in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'plugin',
     'author_company' => 'Ecodev',
     'state' => 'stable',
-    'version' => '3.0.0-dev',
+    'version' => '3.0.1',
     'autoload' =>
         [
             'psr-4' => ['Fab\\VidiFrontend\\' => 'Classes']
