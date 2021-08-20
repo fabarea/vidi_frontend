6ec5f8185f1b8d16e7e57688537bc723ff7997a4
Fabien Udriot <fabien@omic.ch>
2021-08-20 10:49:40 +0200

    Release 4.0.0 - <https://github.com/fabarea/vidi_frontend/compare/3.1.0...4.0.0>

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,15 +8,15 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'plugin',
     'author_company' => 'Ecodev',
     'state' => 'stable',
-    'version' => '3.1.0-dev',
+    'version' => '4.0.0',
     'autoload' =>
         [
             'psr-4' => ['Fab\\VidiFrontend\\' => 'Classes']
         ],
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
-            'vidi' => '4.0.0-0.0.0',
+            'typo3' => '10.4.0-10.4.99',
+            'vidi' => '5.0.0-0.0.0',
         ],
         'conflicts' => [
         ],
