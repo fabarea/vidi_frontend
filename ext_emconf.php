a8148f7af4d085da600726fd6c96c4f90a97a14b
Fabien Udriot <fabien@omic.ch>
2021-08-20 10:50:05 +0200

    [TASK] Prepare next release

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'plugin',
     'author_company' => 'Ecodev',
     'state' => 'stable',
-    'version' => '4.0.0',
+    'version' => '4.0.0-dev',
     'autoload' =>
         [
             'psr-4' => ['Fab\\VidiFrontend\\' => 'Classes']
