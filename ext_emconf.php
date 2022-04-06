1a89d40ca58cb73442eaaa38740feb589b0faede
Fabien Udriot <fabarea@users.noreply.github.com>
2022-04-06 11:59:06 +0200

    [TASK] Update TYPO3 requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '10.4.0-11.5.99',
             'vidi' => '5.0.0-0.0.0',
         ],
         'conflicts' => [
