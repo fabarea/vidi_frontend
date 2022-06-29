2620a5490ec10f2faf7c915a25516899136a6eae
Fabien Udriot <fabien@omic.ch>
2022-06-29 14:12:02 +0200

    Release 5.0.1 - <https://github.com/fabarea/vidi_frontend/compare/5.0.0...5.0.1>

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 5.0.1 (2022-06-29)
+
+[REFACTIR] TYPO3 v11 compatibility follow-up
+[TASK] Prepare next release
+
--- ext_emconf.php
+++ ext_emconf.php
@@ -5,8 +5,6 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Generic List Component for the Frontend where content can be filtered in an advanced way... Veni, vidi, vici!',
     'author' => 'Fabien Udriot',
     'author_email' => 'fabien@ecodev.ch',
-    'category' => 'plugin',
     'author_company' => 'Ecodev',
     'state' => 'stable',
-    'version' => '5.1.0-dev',
 ];
